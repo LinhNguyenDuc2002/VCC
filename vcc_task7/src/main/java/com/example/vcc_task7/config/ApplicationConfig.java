76725b9f536df7f9000be470cc2c82025ef19157
LinhNguyen <linhlinhlinh16042002@gmail.com>
2024-07-10 14:50:25 +0700

    update

--- vcc_task7/src/main/java/com/example/vcc_task7/config/ApplicationConfig.java
+++ vcc_task7/src/main/java/com/example/vcc_task7/config/ApplicationConfig.java
@@ -1,18 +1,32 @@
-//package com.example.vcc_task4.config;
+package com.example.vcc_task4.config;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Configuration
+public class ApplicationConfig {
+    private Logger logger = LogManager.getLogger(ApplicationConfig.class);
+
+//    @Bean
+//    public AsyncTaskExecutor initTaskExecutor() {
+//        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+//        pool.setCorePoolSize(corePoolSize);
+//        pool.setMaxPoolSize(maxPoolSize);
+//        pool.setQueueCapacity(queueCapacity);
+//        pool.setWaitForTasksToCompleteOnShutdown(true);
+//        pool.setThreadNamePrefix("task-");
 //
-//import org.apache.logging.log4j.Level;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//
-//@Configuration
-//public class ApplicationConfig {
-//    private Logger logger = LogManager.getLogger(ApplicationConfig.class);
-//
+//        return pool;
+//    }
+
 //    @Bean
 //    public Connection getConnection() {
 //        Connection connection = null;
@@ -29,4 +43,4 @@
 //            return null;
 //        }
 //    }
-//}
+}
